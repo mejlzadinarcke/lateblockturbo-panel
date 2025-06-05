b85549a9268811562976ea3d0a81649aba04086f
mejlzadinarcke <mejlzadinarcke@gmail.com>
2025-06-05 19:35:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-
-import React, { useEffect, useState } from "react";
-import './App.css';
+import React from 'react';
 
 function App() {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      fetch("/api/bot-status")
-        .then(res => res.json())
-        .then(setData);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="App">
+    <div>
       <h1>LateBlockTurbo Panel</h1>
-      <p>Status: {data.status}</p>
-      <p>Blok: {data.block}</p>
-      <p>Reakcija: {data.reactionTime} ms</p>
-      <p>Simulacija: {data.simulatedProfit} SOL</p>
-      <pre>{data.logs}</pre>
     </div>
   );
 }
